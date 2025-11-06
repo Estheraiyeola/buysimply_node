80d3dfa4af4d10c82d632a7c4c9f451588fa2197
Estheraiyeola <estheraiyeola@yahoo.com>
2025-11-06 09:38:50 +0100

    Enable CORS for Vercel frontend

--- server.js
+++ server.js
@@ -10,6 +10,12 @@ const app = express()
 app.use(express.json())
 app.use(cors())
 
+app.use(cors({
+  origin: ["https://buysimply-vue.vercel.app"],
+  methods: ["GET", "POST", "PUT", "DELETE"],
+  credentials: true
+}))
+
 app.use((req, res, next) => {
     console.log(new Date().toISOString(), req.method, req.path)
     next()
